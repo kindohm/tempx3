a0c43d2e6c71887cbe0a1251f7254c3dd14e4002
Mike Hodnick <mike@kindohm.com>
2021-11-21 12:27:01 -0600

    update hook ref

--- api/githubHook.js
+++ api/githubHook.js
@@ -1,2 +1,2 @@
 var hookshot = require('hookshot');
-hookshot('refs/heads/master', 'git pull && yarn deploy:update').listen(3333)
+hookshot('refs/heads/main', 'git pull && yarn deploy:update').listen(3333)
