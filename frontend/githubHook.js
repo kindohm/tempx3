c495f4c42b62b60b286881e47893596d2e7330d1
Mike Hodnick <mike@kindohm.com>
2021-11-22 08:54:55 -0600

    top level deploy

--- frontend/githubHook.js
+++ frontend/githubHook.js
@@ -1,2 +1,2 @@
-let hookshot = require('hookshot');
-hookshot('refs/heads/main', 'git pull && yarn deploy').listen(3334)
+// let hookshot = require('hookshot');
+// hookshot('refs/heads/main', 'git pull && yarn deploy').listen(3334)
