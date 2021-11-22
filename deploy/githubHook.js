6bf38eea5712f537d7118b4c30aef9e2bc65a563
Mike Hodnick <mike@kindohm.com>
2021-11-22 08:55:27 -0600

    fix deploy path

--- deploy/githubHook.js
+++ deploy/githubHook.js
@@ -1,2 +1,2 @@
 let hookshot = require('hookshot');
-hookshot('refs/heads/main', 'git pull && cd api && yarn deploy:update && cd ../frontend && yarn deploy').listen(3333)
+hookshot('refs/heads/main', 'git pull && cd ../api && yarn deploy:update && cd ../frontend && yarn deploy').listen(3333)
